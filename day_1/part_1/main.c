18f81cd2b0e5cd5e9b46489798bbc46344096ef1
Isaac Spencer <22ispencer@gmail.com>
2024-09-17 21:37:32 -0500

    finished day 1 part 1

--- day_1/part_1/main.c
+++ day_1/part_1/main.c
@@ -1,7 +1,34 @@
 #include <stdio.h>
+#define BUF_SIZE 1024
 
 int main() {
-  printf("Hello, world!\n");
+  FILE *input = fopen("input.txt", "r");
+
+  char buffer[BUF_SIZE];
+  int sum = 0;
+
+  while (fgets(buffer, BUF_SIZE, input) != NULL) {
+    int first = -1;
+    int second = -1;
+
+    for (int i = 0; buffer[i] != '\0'; i++) {
+      if ('1' <= buffer[i] && buffer[i] <= '9') {
+        if (first == -1) {
+          first = buffer[i];
+        }
+        second = buffer[i];
+      }
+    }
+    sum += (first - '0') * 10 + (second - '0');
+  }
+
+  if (feof(input) != 0) {
+    printf("%d\n", sum);
+  } else {
+    printf("error with fgets\n");
+  }
+
+  fclose(input);
 
   return 0;
 }
